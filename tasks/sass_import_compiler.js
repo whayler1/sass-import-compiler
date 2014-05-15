b9b8c46990c794138a621a9a19dcb41cf9147a0e
Justin Worsdale <whayler1@gmail.com>
2014-05-15 16:36:25 -0400

    yeoman grunt plugin structure

---
+++ tasks/sass_import_compiler.js
@@ -0,0 +1,51 @@
+/*
+ * sass import compiler
+ * 
+ *
+ * Copyright (c) 2014 Justin Worsdale
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('sass_import_compiler', 'accepts an array of scss files and writes them to @imports for a main scss file', function () {
+
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function (file) {
+      // Concat specified files.
+      var src = file.src.filter(function (filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function (filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(file.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + file.dest + '" created.');
+    });
+  });
+
+};
