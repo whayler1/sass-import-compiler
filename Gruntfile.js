c9b1aa493c2006745ea16210436d44385bddf728
Justin Worsdale <whayler1@gmail.com>
2014-05-16 14:13:19 -0400

    setting up nodeunit test

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,31 +43,12 @@ module.exports = function (grunt) {
 					]
 				}
 			}
-			
-			/*
-			default_options: {
-				options: {
-				},
-				files: {
-					'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
-				}
-			},
-			custom_options: {
-				options: {
-					separator: ': ',
-					punctuation: ' !!!'
-				},
-				files: {
-					'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
-				}
-			}
-			*/
-		}/*,
+		},
 
 		// Unit tests.
 		nodeunit: {
 			tests: ['test/*_test.js']
-		}*/
+		}
 
 	});
 
