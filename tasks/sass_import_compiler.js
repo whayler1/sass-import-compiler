e48e138a3deb8319466f25e9315b5b706a9ba542
Justin Worsdale <whayler1@gmail.com>
2014-05-16 13:54:56 -0400

    writing file paths with correct path relative to the destination

--- tasks/sass_import_compiler.js
+++ tasks/sass_import_compiler.js
@@ -8,16 +8,11 @@
 
 'use strict';
 
-var fs = require('fs');
-
-/*
-fs.writeFile('foo.js', 'hi there i am foo', function(err) {
-	
-	console.log('hey, sup, did it');
-});
-*/
+var path = require('path');
 
 module.exports = function (grunt) {
+	
+	
 	
 	// Please see the Grunt documentation for more information regarding task
 	// creation: http://gruntjs.com/creating-tasks
@@ -26,44 +21,25 @@ module.exports = function (grunt) {
 
 		// Merge task-specific and/or target-specific options with these defaults.
 		var options = this.options({
-			punctuation: '.',
-			separator: ', '
+			//punctuation: '.',
+			//separator: ', '
 		});
 		
 		// Iterate over all specified file groups.
 		this.files.forEach(function (fileobj) {
 			
-			grunt.log.write('zzz ', fileobj.src.length);
+			var fileContents = '',
+				dest = fileobj.dest,
+				destpath = path.dirname(dest);
 			
 			fileobj.src.forEach(function(filename) {
 				
-				grunt.log.writeln(filename);
+				fileContents += '@import ' + path.relative(destpath, filename).replace(/\.scss/, '') + ';\n';
 			});
 			
-			/*
-			// Concat specified files.
-			var src = file.src.filter(function (filepath) {
-				// Warn on and remove invalid source files (if nonull was set).
-				if (!grunt.file.exists(filepath)) {
-					grunt.log.warn('Source file "' + filepath + '" not found.');
-					return false;
-				} else {
-					return true;
-				}
-			}).map(function (filepath) {
-				// Read file source.
-				return grunt.file.read(filepath);
-			}).join(grunt.util.normalizelf(options.separator));
-
-			// Handle options.
-			src += options.punctuation;
-
-			// Write the destination file.
-			grunt.file.write(file.dest, src);
-
-			// Print a success message.
-			grunt.log.writeln('File "' + file.dest + '" created.');
-			*/
+			grunt.log.writeln(fileContents);
+			
+			grunt.file.write(fileobj.dest, fileContents);
 		});
 	});
 
